bd323d786bbca1c77e0b813f8058c9677479950b
sarketxd <144284224+sarketxd@users.noreply.github.com>
2024-08-24 23:42:51 -0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,29 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import *
+from .views import UsuarioViewSet, CategoriaProductoViewSet, ProductoViewSet, CarritoComprasViewSet, FacturasViewSet, FacturaProductosUsuariosViewSet, ImagenViewSet, InventarioViewSet, SubcategoriaProductoViewSet
 
 router = DefaultRouter()
-router.register(r'addresses', AddressViewSet)
-router.register(r'countries', CountryViewSet)
-router.register(r'order_lines', OrderLineViewSet)
-router.register(r'order_statuses', OrderStatusViewSet)
-router.register(r'payment_types', PaymentTypeViewSet)
-router.register(r'products', ProductViewSet)
-router.register(r'product_categories', ProductCategoryViewSet)
-router.register(r'product_configurations', ProductConfigurationViewSet)
-router.register(r'product_items', ProductItemViewSet)
-router.register(r'promotions', PromotionViewSet)
-router.register(r'promotion_categories', PromotionCategoryViewSet)
-router.register(r'shipping_methods', ShippingMethodViewSet)
-router.register(r'shop_orders', ShopOrderViewSet)
-router.register(r'shopping_carts', ShoppingCartViewSet)
-router.register(r'shopping_cart_items', ShoppingCartItemViewSet)
-router.register(r'site_users', SiteUserViewSet)
-router.register(r'user_addresses', UserAddressViewSet)
-router.register(r'user_payment_methods', UserPaymentMethodViewSet)
-router.register(r'user_reviews', UserReviewViewSet)
-router.register(r'variations', VariationViewSet)
-router.register(r'variation_options', VariationOptionViewSet)
+router.register(r'usuarios', UsuarioViewSet)
+router.register(r'categorias', CategoriaProductoViewSet)
+router.register(r'productos', ProductoViewSet)
+router.register(r'carritos', CarritoComprasViewSet)
+router.register(r'facturas', FacturasViewSet)
+router.register(r'factura-productos', FacturaProductosUsuariosViewSet)
+router.register(r'imagenes', ImagenViewSet)
+router.register(r'inventarios', InventarioViewSet)
+router.register(r'subcategorias', SubcategoriaProductoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
